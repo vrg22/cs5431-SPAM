4c7ba9a9599dc6dd579fa5c5074be7a551367996
Danjurious <danjurious@users.noreply.github.com>
2016-03-07 19:11:36 -0500

    Communications: Client Server Messages Exchange

--- CommClient.java
+++ CommClient.java
@@ -1,8 +1,89 @@
 /**
  * Transfers data between client UI and server communication module
  */
-public interface CommClient {
-    // TODO: decide what type this data should be
-    public void send(Object data);
-    public Object receive();
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+//class CommClient implements Communications {
+public class CommClient {
+	private String hostName;
+	private int portNo;
+	private Socket connection;
+	private ObjectOutputStream commOutputStream;
+	private ObjectInputStream commInputStream;
+
+	private CommClient() {
+		System.out.println("Unused");
+	}
+
+	public CommClient(String host, int port) {
+		this.hostName = host;
+		this.portNo = port;
+	}
+
+	public boolean makeConnection() {
+		boolean status = false;
+		try {
+			InetAddress address = InetAddress.getByName(hostName);
+			connection = new Socket(address, portNo);
+
+			commOutputStream = new ObjectOutputStream(connection.
+					getOutputStream());
+
+			commInputStream = new ObjectInputStream(connection.
+					getInputStream());
+			status = true;
+		} catch (Exception e) {
+			System.err.println("Error connecting to server");
+		}
+		return status;
+	}
+
+	public void send(Message data) {
+		try {
+			commOutputStream.writeObject(data);
+		} catch (Exception e) {
+			System.err.println("Error sending message to server");
+		}
+	}
+
+	public Message receive() {
+		Message msg = null;
+		try {
+			msg = (Message) commInputStream.readObject();
+		} catch (IOException e) {
+			System.err.println("IO Error reading object");
+		} catch (Exception e) {
+			System.err.println("Error reading from the socket");
+		}
+
+		return msg;
+	}
+
+	public void destroyConnection() {
+		try {
+			commOutputStream.close();
+			commInputStream.close();
+			connection.close();
+		} catch (Exception e) {
+			System.err.println("Error closing the connection");
+		}
+	}
+
+	/*
+	 * Uncomment for testing
+	public static void main(String args[]) {
+		CommClient client = new CommClient("localhost", 5998);
+		if (client.makeConnection()) {
+
+			RegisterMessage rm = new RegisterMessage("newuser", "newpassword");
+
+			client.send(rm);
+
+			LoginMessage lm = new LoginMessage("tempuser", "temppassword");
+			client.send(lm);
+			client.destroyConnection();
+		}
+	}*/
 }
