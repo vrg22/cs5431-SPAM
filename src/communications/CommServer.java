c5b8acbe4265d50bece2d3a0d382f83b56637dc4
CK <danjurious@users.noreply.github.com>
2016-03-15 21:12:44 -0400

    CommServer: Multithreaded

--- src/communications/CommServer.java
+++ src/communications/CommServer.java
@@ -7,14 +7,12 @@ package communications;
 import java.util.*;
 import java.io.*;
 import java.net.*;
+import java.lang.Thread;
 
-public class CommServer implements Communications {
+public class CommServer {
 	private String hostName;
 	private int portNo;
-	private Socket connection;
 	private ServerSocket server;
-	private DataOutputStream commOutputStream;
-	private DataInputStream commInputStream;
 
 	private CommServer() {
 		System.out.println("Unused");
@@ -25,145 +23,168 @@ public class CommServer implements Communications {
 		this.portNo = port;
 	}
 
-	public boolean makeConnection() {
+	public void makeConnection() {
 		boolean status = false;
 		try {
 			server = new ServerSocket(portNo);
 
-			/*
-			 * TODO: Move the accept listener to its own thread
-			 */
-			connection = server.accept();
-			commOutputStream = new DataOutputStream(connection.
-					getOutputStream());
-
-			commInputStream = new DataInputStream(connection.
-					getInputStream());
-			status = true;
+			while (true) {
+				ClientWorker w;
+				try {
+					w = new ClientWorker(server.accept());
+					Thread t = new Thread(w);
+					t.start();
+				} catch (Exception e) {
+					System.err.println("Error accepting incoming connection");
+				}
+			}
 		} catch (Exception e) {
 			System.err.println("Error connecting to server");
 		}
-		return status;
-	}
-
-	private byte[] convertToBytes(Message object) throws IOException {
-		try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
-				ObjectOutputStream out = new ObjectOutputStream(bos)) {
-			out.writeObject(object);
-			return bos.toByteArray();
-				}
-	}
-
-	private Message convertFromBytes(byte[] bytes) throws IOException,
-			ClassNotFoundException {
-				try (ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
-						ObjectInputStream in = new ObjectInputStream(bis)) {
-					return ((Message)in.readObject());
-						}
-	}
-
-	public void send(Message m) {
-		try {
-			byte[] message = convertToBytes(m);
-			sendOverNetwork(message, message.length);
-		} catch (IOException e) {
-			System.err.println("Error sending over network");
-		}
-	}
-
-	public Message receive() {
-		try {
-			byte[] message = recvOverNetwork();
-			if (message != null) {
-				Message m = convertFromBytes(message);
-				return m;
-			} else {
-				return null;
-			}
-		} catch (IOException e) {
-			System.err.println("IOException receiving messages over network");
-		} catch (ClassNotFoundException e) {
-			System.err.println("ClassNotFoundException receiving messages over network");
-		}
-
-		return null;
-	}
-
-	public void sendOverNetwork(byte[] data, int len) {
-		try {
-			if (len > 0) {
-				commOutputStream.writeInt(len);
-				commOutputStream.write(data, 0, len);
-			}
-		} catch (Exception e) {
-			System.err.println("Error sending message to server");
-		}
-	}
-
-	public byte[] recvOverNetwork() {
-		try {
-			int len = commInputStream.readInt();
-			//System.out.println("len :" +len);
-			if (len > 0) {
-				byte[] msg = new byte[len];
-				commInputStream.read(msg, 0, len);
-				return msg;
-			}
-		} catch (IOException e) {
-			System.err.println("IO Error reading object");
-		} catch (Exception e) {
-			System.err.println("Error reading from the socket");
-		}
-		return null;
 	}
 
 	public void destroyConnection() {
 		try {
-			commOutputStream.close();
-			commInputStream.close();
-			connection.close();
 			server.close();
 		} catch (Exception e) {
 			System.err.println("Error closing the connection");
 		}
 	}
 
+	private class ClientWorker implements Runnable, Communications {
+		private DataOutputStream commOutputStream;
+		private DataInputStream commInputStream;
+		private Socket sock;
+
+		public ClientWorker(Socket sock) {
+			this.sock = sock;
+			try {
+				commOutputStream = new DataOutputStream(sock.getOutputStream());
+				commInputStream = new DataInputStream(sock.getInputStream());
+			} catch (IOException e) {
+				System.err.println("Error getting input output streams");
+			} catch (Exception e) {
+				System.err.println("Exception when setting up ClientWorker");
+			}
+		}
+
+		public void run() {
+			while (true) {
+				Message m = receive();
+				//call the storageandretreive unit
+				// Gets a replyMessage from StorageAndRetreive
+				// send(replyMessage);
+				if (m != null) {
+					if (m.getQuery().equals("REGISTER")) {
+						Message.RegisterMessage rm =
+							(Message.RegisterMessage) m;
+						System.out.println("Received values");
+						System.out.println(rm.getSequence());
+						System.out.println(rm.getVersion());
+						System.out.println(rm.getUsername());
+						System.out.println(rm.getPassword());
+					} else if (m.getQuery().equals("LOGIN")) {
+						Message.LoginMessage lm =
+							(Message.LoginMessage) m;
+						System.out.println("Received values");
+						System.out.println(lm.getSequence());
+						System.out.println(lm.getVersion());
+						System.out.println(lm.getUsername());
+						System.out.println(lm.getPassword());
+						System.out.println(lm.getAttemptsRemaining());
+					}
+				} else {
+					try {
+						sock.close();
+					} catch (IOException e) {
+						System.err.println("Error closing the connection");
+					}
+					break;
+				}
+
+			}
+		}
+
+		private byte[] convertToBytes(Message object) throws IOException {
+			try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+					ObjectOutputStream out = new ObjectOutputStream(bos)) {
+				out.writeObject(object);
+				return bos.toByteArray();
+					}
+		}
+
+		private Message convertFromBytes(byte[] bytes) throws IOException,
+				ClassNotFoundException {
+					try (ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+							ObjectInputStream in = new ObjectInputStream(bis)) {
+						return ((Message)in.readObject());
+							}
+		}
+
+		public void send(Message m) {
+			try {
+				byte[] message = convertToBytes(m);
+				sendOverNetwork(message, message.length);
+			} catch (IOException e) {
+				System.err.println("Error sending over network");
+			}
+		}
+
+		public Message receive() {
+			try {
+				byte[] message = recvOverNetwork();
+				if (message != null) {
+					Message m = convertFromBytes(message);
+					return m;
+				} else {
+					return null;
+				}
+			} catch (IOException e) {
+				System.err.println("IOException receiving messages over network");
+			} catch (ClassNotFoundException e) {
+				System.err.println("ClassNotFoundException receiving messages over network");
+			}
+
+			return null;
+		}
+
+		private void sendOverNetwork(byte[] data, int len) {
+			try {
+				if (len > 0) {
+					commOutputStream.writeInt(len);
+					commOutputStream.write(data, 0, len);
+				}
+			} catch (Exception e) {
+				System.err.println("Error sending message to server");
+			}
+		}
+
+		private byte[] recvOverNetwork() {
+			try {
+				int len = commInputStream.readInt();
+				//System.out.println("len :" +len);
+				if (len > 0) {
+					byte[] msg = new byte[len];
+					commInputStream.read(msg, 0, len);
+					return msg;
+				}
+			} catch (IOException e) {
+				System.err.println("IO Error reading object");
+			} catch (Exception e) {
+				System.err.println("Error reading from the socket");
+			}
+			return null;
+		}
+
+	}
 	/*
 	 * Uncomment for testing
-	public static void main(String args[]) throws IOException,
-		   ClassNotFoundException {
-			   CommServer cs = new CommServer("localhost", 5998);
+	 public static void main(String args[]) throws IOException,
+			ClassNotFoundException {
+				CommServer cs = new CommServer("localhost", 5998);
 
-			   while (true) {
-				   if (cs.makeConnection()) {
-					   while (true) {
-						   Message m = cs.receive();
-
-						   if (m != null) {
-							   if (m.getQuery().equals("REGISTER")) {
-								   Message.RegisterMessage rm =
-									   (Message.RegisterMessage) m;
-								   System.out.println("Received values");
-								   System.out.println(rm.getSequence());
-								   System.out.println(rm.getVersion());
-								   System.out.println(rm.getUsername());
-								   System.out.println(rm.getPassword());
-							   } else if (m.getQuery().equals("LOGIN")) {
-								   Message.LoginMessage lm =
-									   (Message.LoginMessage) m;
-								   System.out.println("Received values");
-								   System.out.println(lm.getSequence());
-								   System.out.println(lm.getVersion());
-								   System.out.println(lm.getUsername());
-								   System.out.println(lm.getPassword());
-								   System.out.println(lm.getAttemptsRemaining());
-							   }
-						   } else {
-							   cs.destroyConnection();
-							   break;
-						   }
-					   }
-				   }
-			   }
-	}*/
+				while (true) {
+					cs.makeConnection();
+				}
+	 } */
 }
