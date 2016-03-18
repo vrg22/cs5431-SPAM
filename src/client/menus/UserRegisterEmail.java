86bf38fddd3a1eb60b1e7c5088679573ada828fe
Danjurious <danjurious@users.noreply.github.com>
2016-03-17 20:29:17 -0400

    Client: Check email pattern for registration

--- src/client/menus/UserRegisterEmail.java
+++ src/client/menus/UserRegisterEmail.java
@@ -3,8 +3,15 @@ package client.menus;
 import client.Menu;
 import communications.*;
 import communications.Message.*;
+import java.util.regex.Pattern;
 
 public class UserRegisterEmail extends Menu {
+	final static Pattern emailPattern = Pattern.
+		compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+
+	public boolean isEmailValid(final String email) {
+		return ((emailPattern.matcher(email).matches()) ? true : false);
+	}
 
 	public UserRegisterEmail() {
         super("UserRegister-Email");
@@ -13,10 +20,14 @@ public class UserRegisterEmail extends Menu {
 
 	@Override
 	public String handleInput(String input) {
-		Message register = new RegisterMessage(input, null);
-		comm.save(register);
+		if (isEmailValid(input)) {
+			Message register = new RegisterMessage(input, null);
+			comm.save(register);
 
-		return "UserRegister-Password";
+			return "UserRegister-Password";
+		} else {
+			return "UserRegister-Email";
+		}
 	}
 
 }
