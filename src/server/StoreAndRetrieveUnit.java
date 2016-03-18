ecc458486091fc6525d856450d94fe264163bb58
Vivek Gaddam <vrg22@cornell.edu>
2016-03-18 02:38:42 -0400

    About to test register pathway

--- src/server/StoreAndRetrieveUnit.java
+++ src/server/StoreAndRetrieveUnit.java
@@ -65,10 +65,10 @@ public class StoreAndRetrieveUnit {
 	        e.printStackTrace();
 	    }
 		
-		//Create users.xml file with the proper setup
+		//Create the main users.xml file with the proper setup
 		// TODO: Eventually, rather than always having to modify something and save the DOM back to disk on every message,
 		// allow to load DOM once and only save it back to disk if ProcessMessage hasn't been called in a while
-		if (createXMLFile(USERS_FILE_LOCATION) == SUCCESS) {
+		if (createMainXMLFile() == SUCCESS) {
 			//DO SOMETHING
 		}
 	}
@@ -79,7 +79,8 @@ public class StoreAndRetrieveUnit {
 	public Response processMessage(Message m) {
 		if (m == null) throw new IllegalArgumentException("No message received.");
 		
-		//TODO: Load DOM from XML file
+		//Load main user DOM from XML file
+		DOM = loadDOM(USERS_FILE_LOCATION);
 		
 		String query = m.getQuery();
 
@@ -105,7 +106,8 @@ public class StoreAndRetrieveUnit {
 			return obliterate((ObliterateMessage)m);
 		}
 
-		//TODO: Save DOM to XML file
+		//Save DOM to XML file
+		saveDOMtoFile(new File(USERS_FILE_LOCATION), DOM); //change so doesn't need FILE passed
 		
 		throw new IllegalArgumentException("Invalid message received.");
 	}
@@ -126,8 +128,15 @@ public class StoreAndRetrieveUnit {
 		// Unpack message, find out who is trying to register
 		String uName = reg_m.getUsername();
 		String pWord = reg_m.getPassword();
+		
+		//the reg_m has uname, need to check if that corresponds to an ID existing on the system
+		//if so, then response should send that back along with code saying already registered
+		//otherwise, check if there is anyone
 
 		// Try to register
+		
+		//Check if 
+		
 		String respCode = "OK"; //TODO: Change this!
 		logger.log(Level.INFO, "User " + uName + " tried to register");
 
@@ -281,46 +290,64 @@ public class StoreAndRetrieveUnit {
 	//Idea of THREADS making diff modifications to the elements....
 
 	/**
-	 * Returns a handle to a newly-setup XML file with the specified name. Assumes the File exists, but is empty when this is called.
-	 * Do not save the file here; DOM will be set and ready for modifications.
+	 * Sets up the primary user XML file at USERS_FILE_LOCATION (if it doesn't exist). Does nothing if already exists.
 	 * Throws exception if file creation failed.
 	 * @return
 	 */
-	private int createXMLFile(String name){
-        File newFile = new File(name);
-		if (!newFile.exists()) {
-	        createDOM();        //Instantiates and prepares the DOM to be saved to disk
-			saveFile(newFile);
+	private int createMainXMLFile() {
+        File mainFile = new File(USERS_FILE_LOCATION);
+		if (!mainFile.exists()) {
+	        createMainDOM();        //Instantiates and prepares the DOM to be saved to disk
+			saveDOMtoFile(mainFile, DOM); //TODO: Make sure this makes DOM null again
 		}
 		
 		//CHANGE THIS
 		return SUCCESS;
 	}
 
+	
 	/**
-	 * Sets a blank DOM up with fields specific to the USERS_FILE_LOCATION
-	 * Assumes that the private field DOM is not yet set.
+	 * Sets up an XML file (for a single user's vault) with the specified name. 
+	 * If file already exists, this simply loads its DOM and returns that.
+	 * Throws exception if file creation failed.
+	 * @return
+	 */
+	private int createXMLFile(int userID, String username, String password) {
+		String filename = Integer.toString(userID);
+		//if (filename.equals(USERS_FILE_LOCATION)) throw new Exception();
+        
+		File newFile = new File(filename);
+		if (!newFile.exists()) {
+	        Document userDOM = createUserDOM(userID, username, password);        //Instantiates and prepares the DOM to be saved to disk
+			saveDOMtoFile(newFile, userDOM);
+		}
+		
+		//CHANGE THIS
+		return SUCCESS;
+	}
+	
+	
+	/**
+	 * Sets the private field DOM with a Document object with fields specific to the USERS_FILE_LOCATION
+	 * Assumes that the private field DOM is not yet set, and overwrites it.
 	 * Throws exception if DOM creation failed.
 	 * @return
 	 */
-	private void createDOM() {
+	private void createMainDOM() {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		try {
 			DocumentBuilder builder = factory.newDocumentBuilder();
 
 			//Create XML base and set DOM
 			StringBuilder xmlStringBuilder = new StringBuilder(); //TODO: Make private variable?
-			usersXMLsetup(xmlStringBuilder);
+			
+			//Specific setup for the main XML users file
+			setupMainXML(xmlStringBuilder);
 			
 			ByteArrayInputStream input =  new ByteArrayInputStream(xmlStringBuilder.toString().getBytes("UTF-8"));
 			
 			DOM = builder.parse(input);
 	        DOM.getDocumentElement().normalize();
-			
-	        //Verify root information
-//	        Element root = DOM.getDocumentElement();
-//	        String temp = root.getNodeName();
-//	        System.out.println("ROOT ELEMENT: " + temp); //root.getAttribute("class"));
 	
 		} catch (ParserConfigurationException e) {
 			// TODO Auto-generated catch block
@@ -336,20 +363,97 @@ public class StoreAndRetrieveUnit {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	/**
+	 * Sets a blank DOM up with fields specific to a new user.
+	 * Throws exception if DOM creation failed.
+	 * @return
+	 */
+	private Document createUserDOM(int ID, String username, String password) {
+		Document newDOM = null;
+		
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		try {
+			DocumentBuilder builder = factory.newDocumentBuilder();
+
+			//Create XML base and set DOM
+			StringBuilder xmlStringBuilder = new StringBuilder(); //TODO: Make private variable?
+			
+			//Specific setup for the main XML users file
+			setupUserXML(xmlStringBuilder, ID, username, password);
+			
+			ByteArrayInputStream input =  new ByteArrayInputStream(xmlStringBuilder.toString().getBytes("UTF-8"));
+			
+			newDOM = builder.parse(input);
+	        newDOM.getDocumentElement().normalize();
+	
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return newDOM;
+	}
+	
 
 	/**
-	 * Save the existing DOM (if it is active) to disk at the file location.
+	 * Load a DOM up from an existing location on the system.
+	 * Throws exception if Document creation failed.
+	 * @return
+	 */
+	private Document loadDOM(String fileLoc) {
+		Document doc = null;
+		
+		try {
+			//Load XML from disk and set DOM
+			File inputFile = new File(fileLoc);
+	        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	        doc = dBuilder.parse(inputFile); //TODO: IF FILE DNE, what exception needs to be caught?
+	        
+			//StringBuilder xmlStringBuilder = new StringBuilder(); //TODO: Make private variable?
+	
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return doc;
+	}
+	
+	
+	/**
+	 * Save the provided DOM to disk at the file location.
 	 * Throws exception if file is null.
 	 * @return //TODO: success code?
 	 */
-	private void saveFile(File file){
+	private void saveDOMtoFile(File file, Document theDOM){
 		if (file == null) throw new IllegalArgumentException("No file received.");
 		
 		TransformerFactory transformerFactory = TransformerFactory.newInstance();
 		Transformer transformer;
 		try {
 			transformer = transformerFactory.newTransformer();
-			DOMSource source = new DOMSource(DOM);
+			DOMSource source = new DOMSource(theDOM);
 			StreamResult streamResult =  new StreamResult(file); //EDIT this part to make sure it doesn't have to be a new file
 			transformer.transform(source, streamResult);
 		} catch (TransformerConfigurationException e) {
@@ -372,27 +476,161 @@ public class StoreAndRetrieveUnit {
 	/**
 	 * Take in a StringBuilder and append to it the bare bones text necessary for the users XML file.
 	 */
-	private void usersXMLsetup(StringBuilder sb){
+	private void setupMainXML(StringBuilder sb) {
 		//xmlStringBuilder.append("<?xml version=\"1.0\"?>\n<class>\nCAN YOU READ THIS?\n</class>");
 		
 		String basicText =
 			            "<?xml version=\"1.0\"?>\n"
-			            + "<class>\n"
-			            + 	"CAN YOU READ THIS?\n"
-			            + "</class>"
+			            + "<usersXML>\n"
+			            + 	"<metadata>\n"
+			            + 		"<nextID>0</nextID>\n"
+			            + 	"</metadata>\n"
+			            + 	"<users>\n" //EMPTY RN!
+			            + 	"</users>\n"
+			            + "</usersXML>"
+			            ;
+		
+		sb.append(basicText);
+	}
+	
+	/**
+	 * Take in a StringBuilder and append to it the bare bones text necessary for a particular user's XML file.
+	 */
+	private void setupUserXML(StringBuilder sb, int ID, String username, String password) {
+		String newID = Integer.toString(ID);
+		
+		String basicText =
+			            "<?xml version=\"1.0\"?>\n"
+			            + "<user ID=\"" + newID + "\">\n"
+			            + 	"<username>" + username + "</username>\n"
+			            + 	"<password>" + password + "</password>\n"
+			            + 	"<vault></vault>\n" //ADD RECORDS HERE!
+			            + "</user>"
 			            ;
 		
 		sb.append(basicText);
 	}
 	
 	
+	/**
+	 * Look at the loaded DOM and print out ...
+	 * TODO: Exception handling
+	 */
+	private void printDOM() {
+		//Verify DOM information
+        Element root = DOM.getDocumentElement();
+        String rootString = root.getNodeName();
+        System.out.println("ROOT ELEMENT: " + rootString); //root.getAttribute("class"));
+        
+        //Add users to the DOM
+        //...
+        
+        //Iterate through Users (check)
+        ArrayList<User> userList = getUsers();
+        for (User u : userList) {
+        	System.out.println("Username: " + u.getUsername());
+        }
+    }
 	
+	/**
+	 * Adds a user to the main XML file represented by DOM
+	 * TODO: Exception handling
+	 * @return TODO: Success code? or Nothing?
+	 */
+	private void addUser(String uname, String pword) {
+		//TODO: Avoid duplicates before adding this guy?
+		
+		//Directly obtain the "nextID" element (within the "metadata" tag), increment
+		Element idElement = getTagElement("nextID");
+		int nextID = Integer.parseInt(idElement.getTextContent());
+		
+		//Add the appropriate tags underneath parent "users" tag
+		// Get the element
+		// Add child, one row with the three attributes in that order
+		Element uElement = getTagElement("users");
+		
+		Element newUser = DOM.createElement("user");
+		newUser.setAttribute("id", Integer.toString(nextID));
+		Node usrnm = DOM.createTextNode("username");
+		usrnm.setTextContent(uname);
+        newUser.appendChild(usrnm);
+        
+        Node pwd = DOM.createTextNode("password");
+		pwd.setTextContent(pword);
+        newUser.appendChild(pwd);
+        
+        uElement.appendChild(newUser);
+		
+		//Create new user's own XML file. Save to disk.
+        createXMLFile(nextID, uname, pword);
+        Document doc = createUserDOM(nextID, uname, pword);
+        saveDOMtoFile(new File(Integer.toString(nextID)), doc);
+		
+        //Update nextID in main DOM
+        nextID++;
+		idElement.setTextContent(Integer.toString(nextID));
+	}
+	
+	/**
+	 * Return ArrayList of current users from loaded DOM. //Or null if no users yet?
+	 * TODO: Exception handling
+	 * @return 
+	 */
+	private ArrayList<User> getUsers() {
+		//ArrayList of users to be returned
+		ArrayList<User> users = new ArrayList<User>();
+		
+		//Attributes of each user
+		String username;
+		String password;
+		int id;
+
+		//Get the "users" element
+		Element usersElement = getTagElement("users");
+
+	    //Iterate through all child nodes of "users" Element
+    	for (Node n = usersElement.getFirstChild(); n != null; n = n.getNextSibling()) {
+        	//System.out.println("\nCurrent Element: " + uNode.getNodeName());
+    		
+    		//Add this user to our list
+    		if (n.getNodeType() == Node.ELEMENT_NODE) {
+            	Element uElt = (Element) n;
+            	id = Integer.parseInt(uElt.getAttribute("ID"));
+            	
+            	username = uElt.getAttribute("username"); //TODO: Make element
+            	password = uElt.getAttribute("password"); //TODO: Make element
+            	User u = new User(username, password, id);
+            	
+            	users.add(u);
+            }
+    	}
+        
+        return users;
+	}
+	
+	
+	/**
+	 * Return Element corresponding to the specified tag. Expects tagname corresponding to exactly one Node in DOM.
+	 * TODO: Exception handling
+	 * @return 
+	 */
+	private Element getTagElement(String tagname) {
+	    NodeList nlist = DOM.getElementsByTagName(tagname); //TODO: Is it better to get what you want directly by 
+	    Node n = nlist.item(0);
+		Element elt = (Element) n;
+		return elt;
+	}
 	
 	
 	//Testing
 	public static void main(String[] args) {
 		System.out.println("TESTING SRU...\n");
 		StoreAndRetrieveUnit sru = new StoreAndRetrieveUnit(); //Creates the necessary files at startup IF they don't already exist
+		
+		//TODO: Abstract so that can have multiple "DOM"s to handle different XML files. However, the main DOM is the usersDOM
+		sru.DOM = sru.loadDOM(USERS_FILE_LOCATION);
+		//DO SOMETHING WITH LOADED DOM; ex: retrieve ID?
+		sru.printDOM();
 	}
 
 }
