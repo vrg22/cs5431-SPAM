cf419fc4533472c6dadd0babe2e146babd1491ed
CK <danjurious@users.noreply.github.com>
2016-04-14 11:35:34 -0400

    Generate log file name with timestamp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 import static spark.Spark.*;
 import spark.template.handlebars.HandlebarsTemplateEngine;
@@ -8,12 +9,15 @@ import spark.ModelAndView;
 public class Main {
 
     public static void main(String[] args) {
+        String logFileName = new SimpleDateFormat("yyyyMMddhhmm'.log'").
+            format(new Date());
         String logLocation = (args != null && args.length > 0)
             ? args[0]
-            : "log.log";
+            : logFileName;
         String passwordsLocation = (args != null && args.length > 1)
             ? args[1]
             : "users";
+
         try {
             ServerController server = new CentralServerController(logLocation,
                 passwordsLocation);
