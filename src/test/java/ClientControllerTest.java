f9d921741f931a73e5027881435376f70645b8d2
Kevin Beaulieu <kevinmbeaulieu@gmail.com>
2016-04-15 19:24:41 -0400

    Comment out unused test classes

--- src/test/java/ClientControllerTest.java
+++ src/test/java/ClientControllerTest.java
@@ -4,15 +4,15 @@ import static org.junit.Assert.*;
 import static spark.Spark.*;
 
 public class ClientControllerTest {
-    @BeforeClass
-    public static void beforeClass() {
-        Main.main(null); // Start server
-    }
-
-    @AfterClass
-    public static void afterClass() {
-        stop(); // Stop server
-    }
+    // @BeforeClass
+    // public static void beforeClass() {
+    //     Main.main(new String[0]); // Start server
+    // }
+    //
+    // @AfterClass
+    // public static void afterClass() {
+    //     stop(); // Stop server
+    // }
 
     // TODO: add test methods
     // @Test
