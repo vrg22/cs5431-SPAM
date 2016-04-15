e7ec2222f60f6d4366c49151d78f26f377993718
CK <danjurious@users.noreply.github.com>
2016-04-14 20:53:40 -0400

    Encrypt system log files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,128 @@
 import java.util.*;
 import java.io.IOException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.UnsupportedEncodingException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import static spark.Spark.*;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import spark.ModelAndView;
 
 public class Main {
+    private static String adminpassphrase;
+    private static String logLocation;
+    private static String systemConfigFile;
+    private static byte[] salt = new byte[16];
+
+    private static Boolean startupRoutine() {
+        // Retreive admin salt from a file, if absent generate and write to file
+        File file = new File(systemConfigFile);
+        Boolean ret = false;
+        try {
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String sa = br.readLine();
+            System.out.println("Read system salt:" +sa);
+            salt = CryptoServiceProvider.b64decode(sa);
+            br.close();
+            fr.close();
+            ret = true;
+        } catch (FileNotFoundException fne) {
+            System.out.println("salt file not found, creating");
+            try {
+                PrintWriter pw = new PrintWriter(file);
+                salt = CryptoServiceProvider.getNewSalt();
+                String saltString = CryptoServiceProvider.b64encode(salt);
+                System.out.println("Generated system salt:" +saltString);
+                pw.println(saltString);
+                pw.close();
+                ret = true;
+            } catch (IOException e) {
+                System.err.println("Error creating config file" + e.getMessage());
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading config file" + e.getMessage());
+        }
+
+        return ret;
+    }
+
 
     public static void main(String[] args) {
         String logFileName = new SimpleDateFormat("yyyyMMddhhmm'.log'").
             format(new Date());
-        String logLocation = (args != null && args.length > 0)
+        logLocation = (args != null && args.length > 0)
             ? args[0]
             : logFileName;
         String passwordsLocation = (args != null && args.length > 1)
             ? args[1]
             : "users";
+        adminpassphrase = (args != null && args.length > 0)
+            ? args[2]
+            : "defaultadminpassforencryption";
+        systemConfigFile = (args != null && args.length > 0)
+            ? args[3]
+            : "systemconfig.txt";
+
+        Runtime.getRuntime().addShutdownHook(new Thread()
+        {
+            @Override
+            public void run() {
+                System.out.println("Shutdown Hook: adminpassphrase -- " + adminpassphrase);
+                System.out.println("Shutdown Hook: logLocation -- " + logLocation);
+
+                try {
+                    File file = new File(logLocation);
+                    FileInputStream fis = new FileInputStream(file);
+                    byte[] data = new byte[(int) file.length()];
+                    fis.read(data);
+                    fis.close();
+                    String str = new String(data, "UTF-8");
+                    System.out.println("Shutdown Hook: str -- " + str);
+                    String encrypted = CryptoServiceProvider.encrypt(str, adminpassphrase,
+                                        salt);
+                    String curIV = CryptoServiceProvider.b64encode(CryptoServiceProvider.
+                                    getIV());
+
+                    // Escape / with _ and + with - to generate valid file names
+                    curIV.replace('/', '_');
+                    curIV.replace('+', '-');
+
+                    // Save the log file with the IV used to encrypt as its name
+                    File enclog = new File(curIV + ".enclog");
+                    PrintWriter pw = new PrintWriter(enclog);
+                    pw.write(encrypted);
+                    pw.close();
+
+                    // delete current log file
+                    if (true) {
+                    //if (file.delete()) {
+                        System.out.println("Deleted log file:" + logLocation);
+                    } else {
+                        System.err.println("Error deleting log file" +logLocation);
+                    }
+                } catch (FileNotFoundException fne) {
+                    System.err.println("FileNotFoundException raised" + fne.getMessage());
+                } catch (UnsupportedEncodingException use) {
+                    System.err.println("UnsupportedEncodingException raised" + use.getMessage());
+                } catch (IOException e) {
+                    System.err.println("IOException raised" + e.getMessage());
+                }
+            }
+        });
 
         try {
-            ServerController server = new CentralServerController(logLocation,
-                passwordsLocation);
-            new ClientController(server);
+            if (startupRoutine()) {
+                ServerController server = new CentralServerController(logLocation,
+                    passwordsLocation);
+                new ClientController(server);
+        }
         } catch (SecurityException | IOException e) {
             e.printStackTrace();
             return;
